96baa4ab53f205b23ed90d4c8983cd75d3aa2576
Gerardo Lopez-Fernandez <gerir@evernote.com>
2014-02-27 13:05:05 +0100

    added Trollop-based CLI helper, reworked thresholds helper (now JSON_Thresholds)

--- lib/naplug/helpers.rb
+++ lib/naplug/helpers.rb
@@ -7,27 +7,6 @@ module Naplug
 
   module Helpers
 
-    module Thresholds
-
-      def hashify_json_thresholds(tag,thres_json=nil)
-        thresholds = { tag => {} }
-        plug = nil
-        thres_proc = Proc.new do |json_element|
-          case
-            when (json_element.is_a? String and json_element.match(/\d*:\d*:\d*:\d*/))
-              thresholds[:main][plug] = Hash[Status.states.zip json_element.split(':',-1).map { |v| v.nil? ? nil : v.to_i } ]
-            when Symbol
-              plug = json_element
-            else
-              nil
-          end
-        end
-        JSON.recurse_proc(JSON.parse(thres_json, :symbolize_names => true),&thres_proc) if thres_json
-        thresholds
-      end
-
-    end
-
     module Hashes
 
       # Thx Avdi Grimm! http://devblog.avdi.org/2009/11/20/hash-transforms-in-ruby/
---
+++ lib/naplug/helpers/json_thresholds.rb
@@ -0,0 +1,48 @@
+require 'json'
+require 'awesome_print'
+
+module Naplug
+
+  module Helpers
+
+    module JSON_Thresholds
+
+      def self.included(klass)
+        klass.extend ClassMethods
+      end
+
+      module ClassMethods
+        def hashify_json_thresholds(*threstag)
+          ap threstag, :raw => true
+          tag, thresholds_json, thresholds_hash = case threstag.size
+                                                    when 0, 1
+                                                     [nil, threstag[0], {}]
+                                                    else
+                                                     [threstag[1],threstag[0], {}]
+                                                  end
+          plug = nil
+          thresholds_proc = Proc.new do |json_element|
+            case
+              when (json_element.is_a? String and json_element.match(/\d*:\d*:\d*:\d*/))
+                case tag.nil?
+                  when true
+                    thresholds_hash[plug] = Hash[Status.states.zip json_element.split(':',-1).map { |v| v.nil? ? nil : v.to_i } ]
+                  else
+                    thresholds_hash[tag] = Hash[plug, Hash[Status.states.zip json_element.split(':',-1).map { |v| v.nil? ? nil : v.to_i } ]]
+                end
+              when Symbol
+                plug = json_element
+              else
+                nil
+            end
+          end
+          JSON.recurse_proc(JSON.parse(thresholds_json, :symbolize_names => true),&thresholds_proc) if thresholds_json
+          thresholds_hash
+        end
+      end
+
+    end
+
+  end
+
+end
